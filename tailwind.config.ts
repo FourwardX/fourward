38668ad6d45717f771df1c77e8a3dee2900ee91f
SPWwj <wei_dev@outlook.com>
2023-09-11 15:57:18 +0800

    Init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx,scss}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx,scss}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx,scss}',
   ],
   theme: {
     extend: {
@@ -12,6 +12,9 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'space-blue': '#0077CC',
       },
     },
   },
