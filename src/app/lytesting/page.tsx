700634921fbe15a1b72b2d7832a9e63118427a9f
LuoYu-uwu <E0968957@u.nus.edu>
2023-09-29 17:08:29 +0800

    Added a new testing page

---
+++ src/app/lytesting/page.tsx
@@ -0,0 +1,9 @@
+const Page: React.FC = () => {
+  console.log('rendering page @ server');
+  return (
+    <div className="container mx-auto min-h-screen storybook-page">
+    </div>
+  );
+};
+
+export default Page;
